80a51db270d19819604b12e56e49ae6656b34e94
Vespertine13 <eivind.kb@hotmail.com>
2023-04-10 10:42:06 +0200

    functionality works

--- funcs.R
+++ funcs.R
@@ -21,6 +21,22 @@ create_df <- function(folder_a_path, folder_b_path, folder_c_path){
     return(df)
 }
 
+# uses loop
+calculate_hash_freq <- function(df){
+    folder_idx <- colnames(df) %>% grep(pattern ="folder_")
+    hash_df <- df[,folder_idx]
+    hash_df[hash_df == "missing"] <- NA
+    max_hash <- rep(NA, nrow(hash_df))
+    n_max_hash <- rep(NA, nrow(hash_df))
+    for(i in 1:nrow(hash_df)){
+        table_i <- hash_df[i,] %>% as.character() %>% table()
+        max_hash[i] <- names(table_i)[which.max(table_i)]
+        n_max_hash[i] <- table_i %>% as.numeric() %>% max()
+    }
+    df$max_hash <- max_hash
+    df$n_max_hash <- n_max_hash
+    return(df)
+}
 
 
 
@@ -44,5 +60,39 @@ check_3_strings <- function(string_a, string_b, string_c){
         return("b")
     }else{
         return("all")
+    }
+}
+
+create_shell_cmd <- function(df){
+    df$shell_cmd_a <- NA
+    df$shell_cmd_b <- NA
+    df$shell_cmd_c <- NA
+    for(n in 1:nrow(df)){
+        folder_source <- folders[sample(which(c(df$folder_a[n], df$folder_b[n], df$folder_c[n]) ==df$max_hash[n]), 1)]
+        source <- paste0(get(paste0(folder_source, "_path")), df$files[n])
+        if(df$folder_a[n] != df$max_hash[n]){
+            target <- paste0(folder_a_path, df$files[n])
+            df$shell_cmd_a[n] <- glue("xcopy {source} {target}")
+        }
+        if(df$folder_b[n] != df$max_hash[n]){
+            target <- paste0(folder_b_path, df$files[n])
+            df$shell_cmd_b[n] <- glue("xcopy {source} {target}")
+        }
+        if(df$folder_c[n] != df$max_hash[n]){
+            target <- paste0(folder_c_path, df$files[n])
+            df$shell_cmd_c[n] <- glue("xcopy {source} {target}")
+        }
+    }
+    df$shell_cmd_a <- gsub("/", "\\\\", df$shell_cmd_a)
+    df$shell_cmd_b <- gsub("/", "\\\\", df$shell_cmd_b)
+    df$shell_cmd_c <- gsub("/", "\\\\", df$shell_cmd_c)
+    return(df)
+}
+
+run_shells  <- function(df){
+    for(i in 1:nrow(df)){
+        if(!is.na(df$shell_cmd_a[i])){shell(paste0(df$shell_cmd_a[i], "/y /i /f"))}
+        if(!is.na(df$shell_cmd_b[i])){shell(paste0(df$shell_cmd_b[i], "/y /i /f"))}
+        if(!is.na(df$shell_cmd_c[i])){shell(paste0(df$shell_cmd_c[i], "/y /i /f"))}
     }
 }
--- run.R
+++ run.R
@@ -8,66 +8,19 @@ library(glue)
 source("funcs.R")
 
 # paths to different folders
-folder_a_path <- "C:/Users/eivin/AppData/Roaming/folder_a/"
-folder_b_path <- "C:/Users/eivin/AppData/Roaming/mega/folder_b/"
+folder_a_path <- "e:/folder_a/"
+folder_b_path <- "e:/folder_b/"
 folder_c_path <- "e:/folder_c/"
 
-folders <- c("A", "B", "C")
+folders <- c("folder_a", "folder_b", "folder_c")
 
-overview <- create_df(folder_a_path, folder_b_path, folder_c_path)
-
-overview <- fill_hash(overview, folder_a_path, folder_b_path, folder_c_path)
-
-folder_idx <- colnames(overview) %>% grep(pattern ="folder_")
-hash_df <- overview[,folder_idx]
-hash_df[hash_df == "missing"] <- NA
-hash_table <- apply(hash_df, 1, table)
-max_hash <- as.character(lapply(hash_table, function(x) names(x)[which.max(x)])) # denne funker ikke og må byttes
-n_max_hash <- as.numeric(lapply(hash_table, max))
-overview$max_hash <- max_hash
-overview$n_max_hash <- n_max_hash
-
-hash_df
-hash_table 
+folders_df <- create_df(folder_a_path, folder_b_path, folder_c_path)
+overview <- fill_hash(folders_df, folder_a_path, folder_b_path, folder_c_path)
 
 
+freq_df <- calculate_hash_freq(overview)
 
-folder_idx <- colnames(overview) %>% grep(pattern ="folder_")
-overview$message <- NA
-for(n in 1:nrow(overview)){
-    filename <- overview$files[n]
-    print(filename)
-    folder_without <- folders[!(overview[n,folder_idx] %in% overview$max_hash[n])]
-    if(overview$n_max_hash[n] == 2){
-        overview$message[n] <- glue("move file: {filename} into folder {folder_without}")
-    }else if(overview$n_max_hash[n] == 1){
-        overview$message[n] <- glue("Check all versions of {filename}")
-    }else if(overview$n_max_hash[n] == 3){
-        overview$message[n] <- glue("{filename} ok")
-    }else{overview$message[n] <- "Something is wrong"}
-}
+shell_df <- create_shell_cmd(freq_df)
 
-overview$folder_a_ok <- overview$max_hash == overview$folder_a
-overview$folder_b_ok <- overview$max_hash == overview$folder_b
-overview$folder_c_ok <- overview$max_hash == overview$folder_c
+run_shells(shell_df)
 
-overview
-
-# coded from phone
-# plan: one source per file, create path  # randomly from either viable location.
-# one column per folder for shell commands
-# loop though each and insert relevant cmd # when needed.
-# then lastly run shell conmands to 
-# cp files
-
-overview$shell_cmd_a <- NA
-overview$shell_cmd_a <- NA
-overview$shell_cmd_a <- NA
-
-source_folder <- folder_with # must be determined in the algorith above
-source <- paste0(source_folder, overview$filename[n])
-
-target_folder <- folder_without # how does one solve multiple folders without?
-target <- paste0(target_folder, overview$filename[n])
-# copy file
-shell_cmd <- glue("cp {source} {target}")
