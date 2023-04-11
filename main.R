73684f1767c0d91946c4b2a57a0188a1afee09aa
Vespertine13 <eivind.kb@hotmail.com>
2023-04-11 19:54:42 +0200

    new paths for home computer

--- main.R
+++ main.R
@@ -14,9 +14,9 @@ print("Done")
 print("------------------------------------------------------------")
 
 # paths to different folders
-folder_a_path <- "c:/Users/ekb/folder_a/"
-folder_b_path <- "c:/Users/ekb/folder_b/"
-folder_c_path <- "c:/Users/ekb/folder_c/"
+folder_a_path <- "e:/folder_a/"
+folder_b_path <- "e:/folder_b/"
+folder_c_path <- "e:/folder_c/"
 print("Current folders")
 print(folder_a_path)
 print(folder_b_path)
