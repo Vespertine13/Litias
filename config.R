a122550a6524c9aafc304eb2f4971d78d56c66e3
Eivind Kjeka Broen <eivind.kb@hotmail.com>
2023-04-27 13:15:56 +0200

    added some documentation

--- config.R
+++ config.R
@@ -1,5 +1,9 @@
-# paths to folders (full path)
-# paths to folders (full path)
+# Please name the paths to the folders with this convention folder_x x can be any letter.
+# the path should end in in a forward slash / 
+# example:
+# folder_a <- "c:/folder_a/"
+# folder_b <- "c:/folder_b/"
+# folder_c <- "c:/folder_c/"
 
 # all
 folder_a <- "c:/Users/ekb/folder_a/"
--- run.R
+++ run.R
@@ -1,8 +1,13 @@
 # run diagnostics
 # remove all files from local environment
 rm(list= ls())
+
+# source main program
 source("main.R")
+
+# get plot of missing and decayed files
 plot_overview(plot_df)
+
 # run shell
 run_shells(df)
-# clear environment
+
